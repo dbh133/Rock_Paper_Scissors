a7a2c0c6c2a5ee92c18f53a4aec8dc2e14936a65
Dylan Hull <dbh133@hotmail.com>
2022-08-24 21:38:33 -0400

    added in play function with all outcomes for W/L

--- scripts/rps.js
+++ scripts/rps.js
@@ -5,10 +5,39 @@ function getComputerChoice() {
 }
 
 function play(playerSelection, computerSelection) {
-	if (
-		(playerSelection == 'Rock' && computerSelection == 'Paper') ||
-		computerSelection == 'Scissors'
+	if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+		console.log(`you both picked ${playerSelection.toLowerCase()}`);
+	} else if (
+		playerSelection.toLowerCase() === 'rock' &&
+		computerSelection.toLowerCase() === 'scissors'
 	) {
 		console.log('Player Wins');
+	} else if (
+		playerSelection.toLowerCase() === 'paper' &&
+		computerSelection.toLowerCase() === 'rock'
+	) {
+		console.log('Player Wins');
+	} else if (
+		playerSelection.toLowerCase() === 'scissors' &&
+		computerSelection.toLowerCase() === 'paper'
+	) {
+		console.log('Player Wins');
+	} else if (
+		playerSelection.toLowerCase() === 'rock' &&
+		computerSelection.toLowerCase() === 'paper'
+	) {
+		console.log('Computer Wins');
+	} else if (
+		playerSelection.toLowerCase() === 'paper' &&
+		computerSelection.toLowerCase() === 'scissors'
+	) {
+		console.log('Computer Wins');
+	} else if (
+		playerSelection.toLowerCase() === 'scissors' &&
+		computerSelection.toLowerCase() === 'rock'
+	) {
+		console.log('Computer Wins');
 	}
 }
+
+let computerChoice = getComputerChoice();
